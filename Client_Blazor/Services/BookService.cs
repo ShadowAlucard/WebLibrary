dd4d8eaa42e30907db28ecf9fda137381c319e3d
Robi Bihon <bihonrobert@gmail.com>
2023-06-01 13:13:45 +0200

    #services

---
+++ Client_Blazor/Services/BookService.cs
@@ -0,0 +1,6 @@
+﻿namespace Client_Blazor.Services
+{
+    public class BookService
+    {
+    }
+}
---
+++ Client_Blazor/Services/IBookService.cs
@@ -0,0 +1,7 @@
+﻿namespace Client_Blazor.Services
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<BookClass>?> GetAllBook();
+    }
+}
