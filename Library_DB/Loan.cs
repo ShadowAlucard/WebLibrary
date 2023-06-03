84196d9672c827d0a5a4e7ef61d6f25480b486d4
asztalosattila007 <71228545+asztalosattila007@users.noreply.github.com>
2023-06-03 15:07:53 +0200

    Book ,LibraryMember and Loan entities added.
    Book.cs ,LibraryMember.cs and Loan.cs added.

---
+++ Library_DB/Loan.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library_DB
+{
+    public class Loan
+    {
+        public class Loan
+        {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int Id { get; set; }
+
+            [Required]
+            public int MemberId { get; set; }
+
+            [Required]
+            public int BookId { get; set; }
+
+            [Required]
+            public DateTime LoanDate { get; set; }
+
+            [Required]
+
+            public DateTime ReturnDeadline { get; set; }
+        }
+    }
+}
