0ab5625f576e1a8c3ef4f09cdfb2ed884ef526d1
Robi Bihon <bihonrobert@gmail.com>
2023-06-08 11:25:53 +0200

    #Services add to program.cs

--- Client_Blazor/Program.cs
+++ Client_Blazor/Program.cs
@@ -1,4 +1,5 @@
 using Client_Blazor;
+using Client_Blazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7027") });
+
+builder.Services.AddScoped<IBookService, BookService>();
 
 await builder.Build().RunAsync();
