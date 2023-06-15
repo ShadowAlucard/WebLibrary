c08c8f77021375b7ea30a11c7922bc72c4b504ce
Somogyi Patrik <patreszkaa@gmail.com>
2023-06-15 17:14:40 +0200

    Book Service created

---
+++ Librarian_Blazor/Services/BookService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Json;
+using Library_DB;
+
+namespace Librarian_Blazor.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BookService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddBookAsync(Book book) =>
+            await _httpClient.PostAsJsonAsync("Books", book);
+
+        public async Task DeleteBookAsync(int id) =>
+            await _httpClient.DeleteAsync($"Books/{id}");
+
+        public async Task<IEnumerable<Book>?> GetAllBooksAsync() =>
+            await _httpClient.GetFromJsonAsync<IEnumerable<Book>?>("Books");
+
+        public async Task<Book?> GetBookByIdAsync(int id) =>
+            await _httpClient.GetFromJsonAsync<Book?>($"Books/{id}");
+
+        public async Task UpdateBookAsync(int id, Book book) =>
+            await _httpClient.PutAsJsonAsync($"Books/{id}", book);
+    }
+}
---
+++ Librarian_Blazor/Services/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using Library_DB;
+
+namespace Librarian_Blazor.Services
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<Book>?> GetAllBooksAsync();
+        
+        Task<Book?> GetBookByIdAsync(int id);
+
+        Task UpdateBookAsync(int id, Book book);
+
+        Task DeleteBookAsync(int id);
+
+        Task AddBookAsync(Book book);
+    }
+}
