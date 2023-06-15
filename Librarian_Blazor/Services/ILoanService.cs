dcc6afbc48d390059f2a173d994488fb6383f0ef
Somogyi Patrik <patreszkaa@gmail.com>
2023-06-15 17:16:13 +0200

    Loan Service created

---
+++ Librarian_Blazor/Services/ILoanService.cs
@@ -0,0 +1,19 @@
+﻿using Library_DB;
+
+namespace Librarian_Blazor.Services
+{
+    public interface ILoanService
+    {
+        Task<IEnumerable<Loan>?> GetAllLoansAsync();
+
+        Task<Loan?> GetLoanByIdAsync(int id);
+
+        Task<IEnumerable<Loan>?> GetAllLoansByMemberNumberAsync(string memberNumber);
+
+        Task UpdateLoanAsync(int id, Loan loan);
+
+        Task DeleteLoanAsync(int id);
+
+        Task AddLoanAsync(Loan loan);
+    }
+}
---
+++ Librarian_Blazor/Services/LoanService.cs
@@ -0,0 +1,33 @@
+﻿using Library_DB;
+using System.Net.Http.Json;
+
+namespace Librarian_Blazor.Services
+{
+    public class LoanService : ILoanService
+    {
+        private readonly HttpClient _httpClient;
+
+        public LoanService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddLoanAsync(Loan loan) =>
+            await _httpClient.PostAsJsonAsync("Loan", loan);
+
+        public async Task DeleteLoanAsync(int id) =>
+            await _httpClient.DeleteAsync($"Loan/{id}");
+
+        public async Task<IEnumerable<Loan>?> GetAllLoansAsync() =>
+            await _httpClient.GetFromJsonAsync<IEnumerable<Loan>?>("Loan");
+
+        public async Task<IEnumerable<Loan>?> GetAllLoansByMemberNumberAsync(string memberNumber) =>
+            await _httpClient.GetFromJsonAsync<IEnumerable<Loan>?>($"Loan/loanedbooks/{memberNumber}");
+
+		public async Task<Loan?> GetLoanByIdAsync(int id) =>
+            await _httpClient.GetFromJsonAsync<Loan?>($"Loan/{id}");
+
+        public async Task UpdateLoanAsync(int id, Loan loan) =>
+            await _httpClient.PutAsJsonAsync($"Loan/{id}", loan);
+    }
+}
