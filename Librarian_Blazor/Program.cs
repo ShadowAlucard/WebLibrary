e59c0e48d095aa1946d907f2b7b26d1f0e153890
Somogyi Patrik <patreszkaa@gmail.com>
2023-06-15 17:18:25 +0200

    Services attached

--- Librarian_Blazor/Program.cs
+++ Librarian_Blazor/Program.cs
@@ -1,4 +1,5 @@
 using Librarian_Blazor;
+using Librarian_Blazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7027") });
+
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<ILibraryMemberService, LibraryMemberService>();
+builder.Services.AddScoped<ILoanService, LoanService>();
+
+builder.Services.AddScoped<TempService>();
 
 await builder.Build().RunAsync();
